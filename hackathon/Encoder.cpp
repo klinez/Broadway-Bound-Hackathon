5a632e0a38ca01d42ef3a83b1798c4a85d10bc65
gibsonde <143742559+gibsonde@users.noreply.github.com>
2025-03-31 01:28:25 -0400

    Update Encoder.cpp

--- hackathon/Encoder.cpp
+++ hackathon/Encoder.cpp
@@ -1,7 +1,12 @@
 //Written by Ben Zeller
 #include <Arduino.h>
+#include "Encoder.h"
 
-Encoder::Encoder(uint8_t clk, uint8_t dt, uint8_t sw) {}
+Encoder::Encoder(uint8_t aState, uint8_t bState, uint8_t buttonState) {
+    pinMode(aState, INPUT_PULLUP);
+    pinMode(bState, INPUT_PULLUP);
+    pinMode(buttonState, INPUT_PULLUP);
+}
 
 int Encoder::selectTemperature(){
     if(prevAState != aState){
