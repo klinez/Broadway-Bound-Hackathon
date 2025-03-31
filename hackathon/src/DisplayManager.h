d0550659f9f8259f949f1aaf6c4902805016e0a4
gibsonde <143742559+gibsonde@users.noreply.github.com>
2025-03-31 04:42:52 -0400

    Update DisplayManager.h

--- hackathon/src/DisplayManager.h
+++ hackathon/src/DisplayManager.h
@@ -1,10 +1,24 @@
 // File: DisplayManager.h
+//Written by ChatGPT, edited heavily by Daly
 
 #ifndef DISPLAY_MANAGER_H
 #define DISPLAY_MANAGER_H
 
-void initDisplay();
-void updateDisplay(float temp1, float temp2, int tempThreshold, int servoAngle, bool overThreshold);
+#include <Arduino.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+class DisplayManager {
+public:
+    DisplayManager(uint8_t width, uint8_t height, TwoWire* wire, int8_t reset);
+    void initDisplay();
+    void updateDisplay(float temp1, float temp2, int tempThreshold);
+
+private:
+    Adafruit_SSD1306 display;
+
+};
 
 #endif // DISPLAY_MANAGER_H
-#pragma once
+   
+
